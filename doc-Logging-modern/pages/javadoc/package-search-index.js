9a44fc4e1bea715992dacf1a5457e714e3169d34
Andrei Olaru <cs@andreiolaru.ro>
2024-11-05 17:58:42 +0200

    Updated docs and outputs.

--- doc-Logging-modern/pages/javadoc/package-search-index.js
+++ doc-Logging-modern/pages/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.xqhs.util.logging"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.xqhs.util.logging"}];updateSearchResults();
